082aee00a9c1f677f42801a55e5fc5af97182580
thaink <thaibkset@gmail.com>
2018-06-13 21:50:39 +0900

    fix errors

--- Mapping_Schedule_Challenge.m
+++ Mapping_Schedule_Challenge.m
@@ -5,5 +5,5 @@ end
 SDFgraph    = SDFgraph_read(inputfile);
 Schedule = Algorithm_2_DLS(SDFgraph,0);
 Schedule_evaluate(SDFgraph, Schedule);
-Schedule_write(SDFgraph, Schedule2, outputfolder);
+Schedule_write(SDFgraph, Schedule, outputfolder);
 end
